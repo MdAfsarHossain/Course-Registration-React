9ea5afa4c50fa61df55bb37aa7a4bdb0893a5cbc
MdAfsarHossain <afsar291722@gmail.com>
2024-10-07 17:01:02 +0600

    Header jsx file added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-center py-5 border-b-2 mb-5">Course Registration</h1>            
+        </div>
+    );
+};
+
+export default Header;
